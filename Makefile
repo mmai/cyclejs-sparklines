c244828757b89e9cba55e9107505569003e1daae
Henri Bourcereau <henri.bourcereau@gmail.com>
2015-12-15 15:22:23 +0100

    build demo

---
+++ Makefile
@@ -0,0 +1,4 @@
+dev:
+	webpack-dev-server -d --progress
+build:
+	webpack --optimize-minimize
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
-var path = require('path');
-var webpack = require('webpack')
-
 module.exports = {
   // cache: false,
-    entry: './demo.js',
-    output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'demo.build.js',
-    },
-    module: {
-        loaders: [{ test: /\.jsx?$/, exclude: /(node_modules)/, loader: 'babel-loader' }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    plugins: []
-};
+  entry: `./demo.js`,
+  output: {
+    path: __dirname,
+    publicPath: `/`,
+    filename: `demo.build.js`,
+  },
+  module: {
+    loaders: [{test: /\.jsx?$/, loader: `babel-loader`}],
+  },
+  resolve: {
+    extensions: [``, `.js`, `.jsx`],
+  },
+  plugins: [],
+}
